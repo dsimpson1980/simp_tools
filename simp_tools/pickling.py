680990fe258bca9c0d63c819d0f2710db81f1ef5
davidsimpson <mapdes@gmail.com>
2015-04-27 02:14:52 +1000

    Added doctstrings

--- simp_tools/pickling.py
+++ simp_tools/pickling.py
@@ -2,10 +2,30 @@ import cPickle
 
 
 def load(filepath):
+    """Load a pickled object from a pickle file using the faster cPickle
+
+    Parameters
+    ----------
+    filepath: str
+        The filepath to extract a pickle from
+
+    Returns
+    -------
+    object
+    """
     with open(filepath, 'rb') as f:
         return cPickle.load(f)
 
 
 def dump(obj, filepath):
+    """Dump the obj to a pickle file using the quicker cPickle
+
+    Parameters
+    ----------
+    obj: object
+        The object to pickle
+    filepath: str
+        The filepath to dump the pickled object to
+    """
     with open(filepath, 'w') as f:
         cPickle.dump(obj, f, cPickle.HIGHEST_PROTOCOL)
