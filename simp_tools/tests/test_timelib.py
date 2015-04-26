88ccc2e51ee519bb1a29b34e14e791cb4749b66e
davidsimpson <mapdes@gmail.com>
2015-04-27 02:08:45 +1000

    Added minutes_in_freq test Added docstrings and doctests

--- simp_tools/tests/test_timelib.py
+++ simp_tools/tests/test_timelib.py
@@ -14,3 +14,15 @@ class TestCoerceDate(unittest.TestCase):
         assert timelib.coerce_time('12:00') == datetime.time(12)
         assert timelib.coerce_time(pandas.Timestamp('12:00')) == datetime.time(12)
         testing.assert_raises(ValueError, timelib.coerce_time, 'str')
+
+
+class TestMinutesInFreq(unittest.TestCase):
+
+    def test_minutes_in_freq(self):
+        assert timelib.minutes_in_freq('D') == 1440
+        assert timelib.minutes_in_freq('H') == 60
+        assert timelib.minutes_in_freq('T') == 1
+        assert timelib.minutes_in_freq('15T') == 15
+        assert timelib.minutes_in_freq('6H') == 360
+        day = pandas.tseries.frequencies.to_offset('D')
+        assert timelib.minutes_in_freq(day) == 1440
